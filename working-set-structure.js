158a44c4da610d5de3eea8db29b00ddca43bc5d5
Caitlin Cassidy <ccassidy@mit.edu>
2017-11-14 17:43:40 -0500

    Beginning of specs for the working set structure.

---
+++ working-set-structure.js
@@ -0,0 +1,63 @@
+console.log('working set structure');
+
+/**
+ * A structure that maintains the working set invariant.
+ * The working set invariant:
+ *   (2) Every element in deque i has a smaller working
+ *       set than every element in deque i+1.
+ *   (1) Element x lies after y in some deque i iff 
+ *       x has a smaller working set than y.
+ */
+class WorkingSetStructure {
+  constructor() {
+    /**
+      A list of AVL trees to store the elements.
+      this.trees[i] has size 2^(2^i)
+      (except the last one, which might be smaller).
+      */
+    this.trees = [];
+
+    /**
+      A list of deques to store the elements.
+      this.deques[i] has size 2^(2^i)
+      (except the last one, which might be smaller).
+      */
+    this.deques = [];
+
+    // For all i, all elements in this.trees[i]
+    //   are in all elemetns in this.deques[i] and
+    //   vice versa.
+    // Each element is in exactly one tree and
+    //   and exactly one deque.
+  }
+
+  ////////////////////////////////////////////////
+  // Helper methods
+  //
+
+  /**
+   * Shift from h to j.
+   * Both h and j are indices of some tree/deque in
+   *   our structure.
+   * The shift will decrease the size of trees[h] and deques[h]
+   *   by 1 and increase the size of trees[j] and deques[j]
+   *   by 1, maintaining the working set invariant.
+   */
+  shift(h, j) {
+
+  }
+
+  ////////////////////////////////////////////////
+  // Public methods
+  //
+
+  // TODO: spec
+  insert(value) {
+
+  }
+
+  // TODO: spec
+  delete(value) {
+
+  }
+}
