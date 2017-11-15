c5342761908af03feaa0a8c39d513251bd96a6d2
Caitlin Cassidy <ccassidy@mit.edu>
2017-11-14 21:41:17 -0500

    Move WorkingSetStructure and AvlNode to same file.

--- working-set.js
+++ working-set.js
@@ -128,3 +128,85 @@ right = new AvlNode(25);
 rootNode.setRightChild(right);
 console.log(rootNode.getHeight()); // 3
 console.log(rootNode.getBalanceFactor()); // 1
+
+
+
+//////////////////////////////////////
+
+/**
+ * A structure that maintains the working set invariant.
+ * The working set invariant:
+ *   (2) Every element in deque i has a smaller working
+ *       set than every element in deque i+1.
+ *   (1) Element x lies after y in some deque i iff 
+ *       x has a smaller working set than y.
+ */
+class WorkingSetStructure {
+  constructor() {
+    /**
+      A list of AVL trees to store the elements.
+      this.trees[i] has size 2^(2^i)
+      (except the last one, which might be smaller).
+      */
+    this.trees = [];
+
+    /**
+      A list of deques to store the elements.
+      this.deques[i] has size 2^(2^i)
+      (except the last one, which might be smaller).
+      */
+    this.deques = [];
+
+    // For all i, all elements in this.trees[i]
+    //   are in all elemetns in this.deques[i] and
+    //   vice versa.
+    // Each element is in exactly one tree and
+    //   and exactly one deque.
+  }
+
+  ////////////////////////////////////////////////
+  // Helper methods
+  //
+
+  /**
+   * Shift from h to j.
+   * Both h and j are indices of some tree/deque in
+   *   our structure.
+   * The shift will decrease the size of trees[h] and deques[h]
+   *   by 1 and increase the size of trees[j] and deques[j]
+   *   by 1, maintaining the working set invariant.
+   */
+  shift(h, j) {
+
+  }
+
+  ////////////////////////////////////////////////
+  // Public methods
+  //
+
+  /**
+   * Insert value into the structure.
+   */
+  insert(value) {
+
+  }
+
+  /**
+   * Delete value from the structure.
+   */
+  delete(value) {
+
+  }
+
+  /**
+   * Search for value in the structure.
+   * Returns null if the value isn't in the structure.
+   */ 
+  search(value) {
+
+  }
+}
+
+var workingSet = new WorkingSetStructure();
+console.log(workingSet.trees);
+console.log(workingSet.deques);
